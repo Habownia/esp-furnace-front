336a28c2e0f96c999bf17331919f95c9b8e2bb2e
Habownia <hubertkrz7@gmail.com>
2023-12-19 10:07:48 +0100

    Responsive table

--- app/table/page.tsx
+++ app/table/page.tsx
@@ -16,66 +16,65 @@ export default async function Table() {
 		const stringifiedDate = getStringifiedDate(date);
 
 		return (
-			<tr key={elem._id.toString()} className='hover'>
-				<th className='text-xl text-purple-200'>{stringifiedDate}</th>
-				<td className='text-xl'>
-					<span className='text-2xl font-bold'>
-						{elem.temperature.value}
-					</span>
+			<tr
+				key={elem._id.toString()}
+				className='hover text-sm sm:text-base md:text-2xl child-px-0'
+			>
+				<th className=' text-purple-200'>{stringifiedDate}</th>
+				<td>
+					<span className=' font-bold'>{elem.temperature.value}</span>
 					°C
 				</td>
-				<td className='text-xl'>
-					<span className='text-2xl font-bold'>
-						{elem.smoke.value[0]}
-					</span>{' '}
+				<td className=''>
+					<span className=' font-bold'>{elem.smoke.value[0]}</span>{' '}
 					ppm
 				</td>
-				<td className='text-xl'>
-					<span className='text-2xl font-bold'>
-						{elem.smoke.value[1]}
-					</span>{' '}
+				<td className=''>
+					<span className=' font-bold'>{elem.smoke.value[1]}</span>{' '}
 					ppm
 				</td>
-				<td className='text-xl'>
-					<span className='text-2xl font-bold'>
-						{elem.smoke.value[2]}
-					</span>{' '}
+				<td className=''>
+					<span className=' font-bold'>{elem.smoke.value[2]}</span>{' '}
 					ppm
 				</td>
 			</tr>
 		);
 	});
+
 	return (
-		<div className='flex bg-zinc-900 '>
-			<table className='table text-center'>
-				<thead>
+		<div className='flex bg-zinc-900 flex-shrink'>
+			<table className='table text-center child-px-0'>
+				<thead className='text-sm sm:text-base md:text-2xl'>
 					<tr>
 						<th>
-							<div className='flex items-center justify-center gap-2 text-xl text-purple-300'>
+							<div className='flex-center gap-2  text-purple-300'>
 								<MdDateRange className='' />
 								Data
 							</div>
 						</th>
-						<th>
-							<div className='flex items-center justify-center gap-2 text-xl hover:text-red-800'>
+						<th className='p-0'>
+							<div className='flex-center gap-2 hover:text-red-800'>
 								<FaTemperatureFull className='' />
-								Temperatura
+								<span className='hidden lg:inline'>
+									Temperatura
+								</span>
+								<span className='lg:hidden'>Temp.</span>
 							</div>
 						</th>
 						<th>
-							<div className='flex items-center justify-center gap-2 text-xl hover:text-yellow-400'>
+							<div className='flex-center gap-2  hover:text-yellow-400'>
 								<GiGasStove />
 								LPG
 							</div>
 						</th>
 						<th>
-							<div className='flex items-center justify-center gap-2 text-xl hover:text-purple-700'>
+							<div className='flex items-center justify-center gap-2 hover:text-purple-700'>
 								<GiCoalWagon />
 								CO
 							</div>
 						</th>
 						<th>
-							<div className='flex items-center justify-center gap-2 text-xl hover:text-blue-500'>
+							<div className='flex items-center justify-center gap-2 hover:text-blue-500'>
 								<WiSmoke />
 								Dym
 							</div>
